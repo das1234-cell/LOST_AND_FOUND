cf2cd73cb2ff2029e13b4098ee419b030eec4d2d
das1234-cell <subhamdas70870@gmail.com>
2026-01-02 17:03:41 +0530

    JavaScript code update

--- script.js
+++ script.js
@@ -19,3 +19,64 @@ const firebaseConfig ={
 // Firebase  On
 const app = initializeApp(firebaseConfig);
 const db = getFirestone(app);
+
+//===========================================================
+// 2. State Management
+//===========================================================
+
+let currentUser = null;
+let items = [];
+let editingItemId = null;
+
+//===========================================================
+// 3. Global Functions
+//===========================================================
+
+// Login ----->
+
+Window.switchLogin = function(role) {
+  window.currentRole = role;
+  document.getElementById('tab-student').className =role === 'student' ? 'tab-active w-1/2 pb-2 text-center transition-colors' : ' tab-inactive  w-1/2 pb-2 text-center transition-colors';
+  document.getElementById('tab-faculty').className =role === 'faculty' ? 'tab-active w-1/2 pb-2 text-center transition-colors' : ' tab-inactive  w-1/2 pb-2 text-center transition-colors';
+
+}
+
+
+//-------Smart Login System (Google Tool Integration ) --------->
+
+window.handleLogin = async function(e){
+  e.preventDefault();
+  const loginBtn = document.querySelector ('#login-from button');
+  const originalText = loginBtn.innerHTML;
+
+  // 1. User Checking .....>
+  loginBtn.innerHTML = '<i class= "fa-solid fa-circle-notch fa-spin"></i> Checking ID...';
+  loginBtn.disable = true;
+
+
+  const name= document.getElementById('user-name').value;
+  const phone=document.getElementById('user-phone').value;
+  const dept=document.getElementById('user-dept').value;
+  const idNum=document.getElementById('user-id').value;
+  const role= window.currentRole ||'student';
+
+
+  try{
+    //2. User Verification... ai number kau ache naki>
+
+    const usersRef = collection(db,'users');
+    const q = query(usersRef, where ('phone',"==", phone));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty){
+      // ---- Old User (Login)---->
+      // Jodi phone Num paowa jay tahole take Old data nia aso-->
+      const userDocs = querySnapshot.docs[0] .data();
+      currentUser = usreData;
+      
+
+    }
+
+  }
+
+}
